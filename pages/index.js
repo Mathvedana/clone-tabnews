9d7c18677e44de66330e1254696f60b53be992b8
Mathvedana <83519800+Mathvedana@users.noreply.github.com>
2023-11-22 22:58:32 +0000

    adiciona a pasta page

---
+++ pages/index.js
@@ -0,0 +1,5 @@
+function Home() {
+    return <h1>eu te amo, voce é muito importante pra mim e pra minha vida, sem voce eu nao teria forças para fazer nada! Meu mundooo </h1>
+}
+
+export default Home;
