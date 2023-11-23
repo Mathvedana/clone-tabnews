742768347e98321154195e47b59ee5c10af312cd
Mathvedana <83519800+Mathvedana@users.noreply.github.com>
2023-11-23 23:01:20 +0000

    modifica a frase da pagina novamente

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>eu te amo, voce é muito importante pra mim e pra minha vida, sem voce eu nao teria forças para fazer nada! Meu mundooo <3 </h1>
+    return <h1>eu te amo, voce é muito importante pra mim e pra minha vida, sem voce eu nao teria forças para fazer nada! Meu mundooo 🧡🧡🧡🧡  </h1>
 }
 
 export default Home;
