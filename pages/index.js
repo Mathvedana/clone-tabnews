0d0a7e57dea78abe2526d976b58a59074856f452
Mathvedana <83519800+Mathvedana@users.noreply.github.com>
2023-11-29 19:22:53 +0000

    nada

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ function Home() {
   return;
   <h1>
     eu te amo, voce é muito importante pra mim e pra minha vida, sem voce eu nao
-    teria forças para fazer nada! Meu mundooo 🧡🧡🧡🧡{" "}
+    teria forças para fazer nada! Meu mundooo 🧡🧡🧡
   </h1>;
 }
 
